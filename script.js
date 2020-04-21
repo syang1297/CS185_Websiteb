20872f076a8776a4b61e3e52de4011dccbed011a
syang1297 <syang01@umail.ucsb.edu>
2020-04-20 22:54:34 -0700

    before comment

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-var scrollTopBtn = document.getElementById("scrollBtn");
+var scrollTopBtn = document.getElementById("#scrollBtn");
 window.onscroll = function(){scrollToTop()};
 
-offset = 100;
-docHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.docElement.clientHeight, document.docElement.scrollHeight, docElement.offsetHeight);
-if(docHeight!= 'undefined'){
-	offset = docHeight/4;
-}
 
-// function scrollToTop(){
-//     scrollPos = document.body.scrollTop || document.documentElement.scrollTop;
-//     if(scrollPos > offset){
-//         scrollTopBtn.className = 'visible';
-//     }else{
-//         scrollTopBtn.className = 
-//     }
-// }
-// window.addEventListener("scroll", function(){
-// 	scrollPos = body.scrollTop || docElement.scrollTop;
-// 	btt.className = (scrollPos > offset)? 'visible' : '';
-// });
-// btt.addEventListener("click", function(){
-// 	event.preventDefault();
-// 	docElement.scrollTop = 0;
-// 	body.scrollTop = 0;
-// });	
-
-//CHANGE TO A QUARTER OF THE PAGE INSTEAD OF 20PX
 function scrollToTop(){
-    if(document.body.scrollTop > 5 || document.documentElement.scrollTop > 5){
-        scrollTopBtn.style.visibility = "visible";
+    console.log(scrollTopBtn);
+    var scrollPos = document.body.scrollTop || document.documentElement.scrollTop;
+    if(scrollPos > .25){
+        scrollTopBtn.className = 'visible';
     }else{
-        scrollTopBtn.style.visibility = "hidden";
+        scrollTopBtn.className = '';
     }
 }
 
@@ -39,15 +17,8 @@ function topFunction() {
     document.documentElement.scrollTop = 0; 
 }
 
-offset = 100,
-body = document.body,
-docElement = document.documentElement,
-scrollPos, docHeight;
-
-
-
-
-function showActiveTab(tab){
+function activeTab(tab){
     var active = document.querySelector(tab);
+    console.log(tab);
     active.style.background = "rgba(150, 37, 80, 0.377)";
 }
