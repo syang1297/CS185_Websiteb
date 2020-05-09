9aed59a0424c856f50e034ee5788ca57d280f8bc
syang1297 <syang1297@gmail.com>
2020-05-08 17:03:52 -0700

    got to read from database on time

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ export class App extends Component{
       })
     }
   }
+  
+
   render(){
     const tabs = [
       {
--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
+import Config from '../Config.js';
+
+const firebase = require('firebase')
+
 
 export class GuestBook extends Component{ 
-  render(){
+    constructor(){
+        super();
+        this.state = {
+            data: [],
+            shouldRender: true
+        }
+    }
+    componentDidMount(){
+        if (!firebase.apps.length) {
+          firebase.initializeApp(Config);
+        } 
+        let ref = firebase.database().ref('data');
+        //retrieve its data
+        if(this.state.shouldRender){
+            ref.on('value', snapshot => {
+                const state = snapshot.val();
+                // this.state.data = state;
+                this.setState({
+                    data: state,
+                    shouldRender: !this.state.shouldRender
+                });
+            });
+        }
+    }
+
+    render(){
     return (
       <div>
         <h2>GuestBook</h2>
         <div>GuesBook page</div>
+        {/* trying to test firebase rn */}
+        <p>{this.state.data}</p>
       </div>
     );
   }
