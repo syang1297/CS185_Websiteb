1623f76f0fc2e3f40b44686a8dae892d03a083af
syang1297 <syang1297@gmail.com>
2020-05-08 18:24:38 -0700

    finished basic UI of form

--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import Config from '../Config.js';
+import Switch from "react-switch";
+
+
+// fix CSS for the guestForm and GuestMessages
 
 const firebase = require('firebase')
 const sample = ["hi", "hello", "how are you?"]
@@ -33,32 +37,39 @@ export class GuestBook extends Component{
 
     render(){
     return (
-      <div className="guestContainer">
-          <div className="guestForm">
-              <form>
-                  <input type="text" name="name" placeholder="What is your name?"/>
-                  <input type="text" name="description" placeholder="What is your description?"/>
-                  <input type="text" name="message" placeholder="What is your message?"/>
-                    {/* missing show message */}
-                  <input type="text" name="email" placeholder="What is your email?"/>
-
-              </form>
-          </div>
-          <div className="guestMessages">
-
-          </div>
-        {/* <h2>GuestBook</h2>
-        <div>GuesBook page</div>
-        {/* trying to test firebase rn */}
-        {/* <p>{this.state.data}</p>
-        {sample.map((s, index) => (
-            <p>
-                {s}
-            </p>
-        ))} */} 
-      </div>
+        <div>
+            <div className="guestTitle">Leave Me a Message!</div>   
+            <div className="guestContainer">
+                <div className="guestForm">
+                    {/* <form className="guestForm"> */}
+                    <div>What is your name?</div>
+                        <input type="text" name="name" placeholder="ex: John Smith"/>
+                    <div>What is your desription?</div>
+                        <input type="text" name="description" placeholder="ex: a friend"/>
+                    <div>What is your message?</div>
+                        <input type="text" name="message" placeholder="ex: Hello! Lovely website!"/>
+                            {/* missing show message */}
+                            {/* missing time */}
+                    <div>What is your email?</div>
+                        <input type="text" name="email" placeholder="ex:johnsmith@email.com"/>
+                    <button className="submitBtn">Submit Message</button>
+                    {/* </form> */}
+                </div>
+                <div className="guestMessages">Some Guest Messages wheee</div>
+            </div>
+        </div>
     );
   }
 }
 
 export default GuestBook;
+
+// <h2>GuestBook</h2>
+//         <div>GuesBook page</div>
+//         {/* trying to test firebase rn */}
+//          <p>{this.state.data}</p>
+//         {sample.map((s, index) => (
+//             <p>
+//                 {s}
+//             </p>
+//         ))} 
