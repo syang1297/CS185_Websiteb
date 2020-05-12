6d5794a5d22953d4d3f2c0e7e614ef7c71135204
syang1297 <syang1297@gmail.com>
2020-05-11 23:52:47 -0700

    got messages to write to db and show up, just need to test edge cases

--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Config from '../Config.js';
 import Switch from "react-switch";
+// import Alert from '@material-ui/lab/Alert';
+
 
 
 // fix CSS for the guestForm and GuestMessages
@@ -13,10 +15,19 @@ export class GuestBook extends Component{
         super();
         this.state = {
             data: [],
-            shouldRender: true
+            shouldRender: true,
+            name: '',
+            description: '',
+            message: '',
+            dropdown: '',
+            email: '',
+            time: ''
             // sample: 
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     componentDidMount(){
         if (!firebase.apps.length) {
           firebase.initializeApp(Config);
@@ -27,12 +38,85 @@ export class GuestBook extends Component{
             ref.on('value', snapshot => {
                 const state = snapshot.val();
                 // this.state.data = state;
+                let newState = [];
+                for(let s in state){
+                    if(state[s].dropdown == "Yes"){
+                        newState.push({
+                            name: state[s].name,
+                            description: state[s].description,
+                            message: state[s].message,
+                            dropdown:state[s].dropdown,
+                            email: state[s].email,
+                            time: state[s].time
+                        });
+                    }
+                }
+                // this.setState({
+                //     data: state,
+                //     shouldRender: !this.state.shouldRender
+                // });
                 this.setState({
-                    data: state,
-                    shouldRender: !this.state.shouldRender
+                    data: newState
                 });
             });
         }
+    }
+
+    //also include the other prop values?
+    handleChange(e) {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        console.log("submit btn pressed");
+        var sub = 1;
+        e.preventDefault();
+        if(this.state.name == ""){
+            // <Alert severity="error">Please fill out missing name information</Alert>
+            alert('Please fill out missing name information');
+            return;
+            // sub = 0;
+        }
+        if(this.state.message == ""){
+            // <Alert severity="error">Please fill out missing message information</Alert>
+            alert('Please fill out missing message information');
+            return;
+            // sub = 0;
+
+        }
+        if(this.state.dropdown == ""){
+            // <Alert severity="error">Please fill out missing dropdown information</Alert>
+            alert('Please fill out missing dropdown information');
+            return;
+            // sub = 0;
+
+        }
+        // if(sub == 1){
+            const ref = firebase.database().ref('data');
+            const msg = {
+              name: this.state.name,
+              description: this.state.description,
+              message: this.state.message,
+              dropdown: this.state.dropdown,
+              email: this.state.email,
+              time: new Date().toLocaleString()
+            }
+            ref.push(msg);
+            this.setState({
+                name: '',
+                description: '',
+                message: '',
+                dropdown:'',
+                email: '',
+                time: ''
+            });
+            // <Alert severity="success">Message recorded!</Alert>
+            alert('Message recorded');
+        // }
+
+
     }
 
     render(){
@@ -43,19 +127,38 @@ export class GuestBook extends Component{
                 <div className="guestForm">
                     {/* <form className="guestForm"> */}
                     <div>What is your name?</div>
-                        <input type="text" name="name" placeholder="ex: John Smith"/>
-                    <div>What is your desription?</div>
-                        <input type="text" name="description" placeholder="ex: a friend"/>
+                        <input type="text" name="name" minLength="5" maxLength="20" placeholder="ex: John Smith" onChange={this.handleChange} value={this.state.name}/>
+                    <div>What is your description?</div>
+                        <input type="text" name="description" maxLength="100" placeholder="ex: a friend" onChange={this.handleChange} value={this.state.description}/>
                     <div>What is your message?</div>
-                        <textarea type="paragraph_text" cols="50" rows="10" name="message" placeholder="ex: Love the new website"/>
-                            {/* missing show message */}
-                            {/* missing time */}
+                        <textarea type="paragraph_text" cols="50" rows="10" minLength="15" maxLength="500" name="message" placeholder="ex: Love the new website" onChange={this.handleChange} value={this.state.message}/>
+                    <div>Would you like your message to show on the site?</div>
+                        <select name="dropdown" onChange={this.handleChange} value={this.state.dropdown}>
+                            <option value = "No">No</option>
+                            <option value="Yes">Yes</option>
+                        </select>
                     <div>What is your email?</div>
-                        <input type="text" name="email" placeholder="ex: johnsmith@email.com"/>
-                    <button className="submitBtn">Submit Message</button>
+                        <input type="text" name="email" placeholder="ex: johnsmith@email.com" onChange={this.handleChange} value={this.state.email}/>
+                    <button className="submitBtn" onClick={this.handleSubmit}>Submit Message</button>
                     {/* </form> */}
                 </div>
-                <div className="guestMessages">Some Guest Messages wheee</div>
+                <div className="guestMessages">
+                          {this.state.data.map((msg) => {
+                            return (
+                            <div className="message">
+                                <div className="messageHeader">
+                                    <div className="messageName">{msg.name}  </div>
+                                    <div className="messageDescription">  --  {msg.description}</div>
+                                </div>
+                                {/* <div className="message"> */}
+                                {/* </div> */}
+                                <div className="messageTime">{msg.time}</div>
+                                <div className="messageMessage">{msg.message}</div>
+                            </div>
+
+                            )
+                        })}
+                </div>
             </div>
         </div>
     );
