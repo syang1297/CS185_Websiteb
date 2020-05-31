f35b9cec03367cf8fbf7147168095edfa4fe8d91
syang1297 <syang1297@gmail.com>
2020-05-30 23:37:13 -0700

    missing checking duplicates

--- src/Components/AddMovie.js
+++ src/Components/AddMovie.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 import Config from '../Config.js';
 
 const firebase = require('firebase')
+const axios = require('axios');
 
 
 export class AddMovie extends Component{ 
   constructor(){
     super();
     this.state = {
-      id: ''
+      // key: '',
+      id: '',
+      img: '',
+      title: '',
+      direction: '',
+      rating: '',
+      movie: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,11 +27,40 @@ export class AddMovie extends Component{
     } 
   }
 
-handleChange(e) {
-    this.setState({
-      id: e.target.value
+  handleChange(e) {
+      this.setState({
+        id: e.target.value
+      });
+  }
+
+
+  getRequest(id){
+    const id1 = 'https://www.omdbapi.com/?i=';
+    const id2 = '&apikey=915b3e92'
+    const idCurrent = id1 + "tt" + id + id2;
+    console.log("logging current get request");
+    axios.get(idCurrent)
+    .then(response =>{
+        console.log("successful call to api");
+        const mov = {
+            // key: '', 
+            id,
+            img: response.data.Poster,
+            title: response.data.Title,
+            director: response.data.Director,
+            rating: response.data.imdbRating
+        }
+        this.setState({movie: [mov]}, this.pushMovie);
+        console.log("logging state in getResponse");
+        console.log(this.state);
+    })
+    .catch(function(error){
+        console.log("unsuccessful call to api")
+    })
+    .then(function(){
+        return;
     });
-}
+  }
 
   handleSubmit(e) {
     console.log("submit btn pressed");
@@ -38,30 +74,37 @@ handleChange(e) {
       alert('Movie ids should be 7 numbers');
       return;
     }
+    this.getRequest(this.state.id);
+  }
 
-    // console.log(this.state.id);
+  pushMovie(){
     const ref = firebase.database().ref('movie');
-    const msg = {
-        id: this.state.id
-    }
-    const id = this.state.id
-    ref.push(msg);
+    
+    console.log("logging this.state");
+    console.log(this.state);
+    ref.push(this.state.movie);
+    console.log("logging this.state.movie");
+    console.log(this.state.movie);
     // ref.child(id).setValue(msg);
     this.setState({
-        movie: ''
+        // key: '',
+        id: '',
+        img: '',
+        title: '',
+        direction: '',
+        rating: '',
+        movie: []
     });
     alert('Movie added');
+
   }
 
   render(){
     return (
       <div>
-        {/* <h2>Add Movie</h2> */}
         <div>New movie IMDb ID (excluding tt): </div>
-        {/* <div>TT</div> */}
         <input type="number" name="movieID" placeholder="ex: 1234567" onChange={this.handleChange} value={this.state.id}/>
         <button className="submitBtn" onClick={this.handleSubmit}>Add Movie</button>
-
       </div>
     );
   }
