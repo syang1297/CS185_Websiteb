36555d57acc434203c1828a52b1b9010fca56193
syang1297 <syang1297@gmail.com>
2020-05-31 06:28:15 -0700

    got lightbox movie list to only show lists they're not on

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -12,7 +12,8 @@ var currentModal = {
     title: '',
     director: '',
     rating: '',
-    id: ''
+    id: '',
+    list: []
 }
 
 var lists = [];
@@ -24,6 +25,7 @@ export class Movies extends Component{
             movies: [],
             lists: [],
             shouldRender: true,
+            shouldRenderList: true,
             shouldLoad: false,
             modalShow: false,
             refresh: false
@@ -34,14 +36,18 @@ export class Movies extends Component{
         this.setState({modalShow: true});
         const alt = e.target.alt;
         var altSplit = alt.split("|");
+        var ID = altSplit[3];
+        var l = this.retrieveMovieList(ID);
         var curr= {
             img: e.target.src,
             title: altSplit[0],
             director: altSplit[1],
             rating: altSplit[2],
-            id: altSplit[3]
+            id: ID,
+            list: l
         }
         currentModal = curr;
+        console.log(currentModal);
     };
     
     hideModal = () => {
@@ -80,15 +86,38 @@ export class Movies extends Component{
             });
             this.setState({shouldLoad: true})
         }
-        this.setState({lists: ["All"]});
         let ref1 = firebase.database().ref('list');
-        ref1.on('value', snapshot=>{
-            const lists = snapshot.val();
-            console.log(lists);
-            for(let l in lists){
-                this.setState({lists: [...this.state.lists, l]});
-            }
-        });
+        // let list = [];
+        if(this.state.shouldRenderList){
+            ref1.on('value', snapshot =>{
+                const lists = snapshot.val();
+                let newList = [];
+                newList.push("All");
+
+                for(let l in lists){
+                    newList.push(
+                        lists[l].title
+                    );
+                }
+                console.log("printing newList");
+                console.log(newList);
+                this.setState({lists: newList, shouldRenderList: false});
+    
+                // this.setState({lists: [...this.state.lists, lists]});
+                // list.push(lists);
+            });
+        }
+        // this.setState({lists: ["All"]});
+        // let ref1 = firebase.database().ref('list');
+        // ref1.on('value', snapshot=>{
+        //     const lists = snapshot.val();
+        //     console.log(lists);
+        //     for(let l in lists){
+        //         this.setState({lists: [...this.state.lists, l]});
+        //     }
+        // });
+        this.retrieveList();
+        // this.retrieveMovieList();
     }
 
     retrieveMovies(){
@@ -126,15 +155,82 @@ export class Movies extends Component{
             firebase.initializeApp(Config);
         } 
         let ref1 = firebase.database().ref('list');
-        ref1.on('value', childSnapshot=>{
-            const lists = childSnapshot.key;
-            this.setState({lists: [...this.state.lists, lists]});
+        // let list = [];
+        if(this.state.shouldRenderList){
+            ref1.on('value', snapshot =>{
+                const lists = snapshot.val();
+                let newList = [];
+                newList.push("All");
+                for(let l in lists){
+                    newList.push(
+                        lists[l].title
+                    );
+                }
+                this.setState({lists: newList, shouldRenderList: false});
+    
+                // this.setState({lists: [...this.state.lists, lists]});
+                // list.push(lists);
+            });
+        }
+
+    }
+
+    retrieveMovieList(id){
+        // console.log("retrieveMovieList called");
+        if (!firebase.apps.length) {
+            firebase.initializeApp(Config);
+        } 
+        let list = this.state.lists;
+                console.log("printing list");
+        console.log(list);
+        if(typeof list === 'undefined' || id === null){
+            return ["All"];
+        }
+
+        let ref = firebase.database().ref('movieListPair');
+        ref.on('value', snapshot => {
+            const state = snapshot.val();
+            for(let s in state){
+                // console.log("printing s");
+                // console.log(state[s].movie);
+                console.log(state[s].movie);
+                console.log(id);
+                if(state[s].movie.trim() === id.trim()){
+
+                    var toRemove = state[s].list;
+                    var index = list.indexOf(toRemove);
+                    if(index > -1 ){
+                        list.splice(index, 1);
+                    }
+                }
+            }
+            var toRemove = "All";
+            var index = list.indexOf(toRemove);
+            if(index > -1){
+                list.splice(index, 1);
+            }
         });
+        console.log("printing returning list");
+        console.log(list);
+        return list;
     }
 
     changeList(e){
-        alert("changeList clicked")
+        // alert("changeList clicked")
         console.log("list changing");
+        console.log(e);
+        console.log(e.value);
+        //display all movies that have that movie list pair
+    }
+
+    addList(e){
+        console.log("add list clicked");
+        console.log(e);
+        console.log(currentModal);
+        firebase.database().ref('movieListPair/' + currentModal.id.trim() + '-' + e.value).set({
+            movie: currentModal.id.trim(),
+            list: e.value
+        });
     }
 
     dimPoster = (e) => {
@@ -146,6 +242,8 @@ export class Movies extends Component{
     }
 
     render(){
+        const unloaded = (<p>Loading movies...</p>)
+
         console.log(this.state.movies);
         const Movies = this.state.movies && 
         this.state.movies.map(({id, img, title, director, rating}) => {
@@ -162,10 +260,30 @@ export class Movies extends Component{
         // this.retrieveList();
         console.log("logging lists");
         console.log(this.state.lists);
-        const defaultOption = this.state.lists[0];
+        // const defaultOption = this.state.lists[0];
+        if(!this.shouldRenderList){
+            var movieList = [];
+            var list = [];
+            list = this.state.lists;
+            console.log("loggings lists");
+            console.log(list);
+            var defaultOption = list[0];
+            if(currentModal.id === null){
+                // movieList.push("All");
+                movieList = ["All"];
+            }else{
+                movieList = this.retrieveMovieList(currentModal.id);
+                console.log("printing movielist");
+                console.log(movieList);
+            }
+        }else{
+            return unloaded;
+        }
+        
         const loaded = (
             <div>
-                <Dropdown options={this.state.lists} onChange={this.changeList} value={defaultOption} placeholder="All"/>
+                <Dropdown options={list} onChange={this.changeList} value={defaultOption} placeholder="All"/>
+                {/* list.map(()) */}
                 <div className="parent-grid">
                     {Movies}
                     <Modal modalShow={this.state.modalShow} handleClose={this.hideModal} handleDelete={this.deleteMovie}>
@@ -178,7 +296,7 @@ export class Movies extends Component{
                                 </div>
                                 <div>
                                     <p>Add to list:</p>
-                                    <Dropdown options={this.state.lists} onChange={this.changeList} value={defaultOption} placeholder="All"/>
+                                    <Dropdown options={currentModal.list} onChange={this.addList} value={defaultOption} placeholder="All"/>
                                 </div>
                             </div>
 
@@ -186,9 +304,7 @@ export class Movies extends Component{
                 </div>
             </div>
         );
-        const unloaded = (<p>Loading movies...</p>)
 
-        // return loaded;
         if(this.state.shouldLoad) return loaded
         return unloaded
   }
